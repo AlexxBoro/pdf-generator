30ea97d07c6c57cebd8c34e77bfb09988a3a9667
AlexxBoro <aleks.borowska91@gmail.com>
2021-06-18 14:40:45 +0200

    added button for PDF generation

--- myapp/pdfgenerator/views.py
+++ myapp/pdfgenerator/views.py
@@ -1,42 +1,95 @@
 from django.http import HttpResponse
 from django.http import HttpResponse
 from django.template import loader
+from django.conf import settings
 from weasyprint import HTML, CSS
-import tempfile
+from django.http import JsonResponse
 
 
 def index(request):
-    return HttpResponse("PDF generator app!")
+    html_string = loader.render_to_string("index.html")
+
+    return HttpResponse(html_string)
 
 
 def generate_pdf(request):
     """Generate pdf."""
-    # Model data
-    # people = [
-    #     {"name": "Ola", "surname": "B", "age": "29"},
-    #     {"name": "Basia", "surname": "K", "age": "25"},
-    # ]
+
+    input_data = {
+        "name": "Wolt: Tue paikallisia ravintoloita",
+        "used_budget": 6562,
+        "budget": 3000,
+        "creators": 24,
+        "submissions": 48,
+        "applications": 242,
+        "reach": 232874,
+        "engagement": 17786,
+        "impressions": 252791,
+        "cpm": 25.96,
+        "cpe": 0.37,
+        "engagement_percentage": 10.28,
+        "followers": 271389,
+        "audience_female_share": 0.65,
+        "top_posts": [
+            {
+                "awareness": 100.0,
+                "compatibility": 100.0,
+                "virality": 51.6,
+                "engagement": 50.0,
+                "image": "https://cdn.boksi.com/.../thumbnail.jpg",
+                "creator": {
+                    "name": "Jutta Häkämies",
+                    "username": "juttaemmii",
+                    "image": "https://cdn.boksi.com/.../thumbnail.jpg",
+                },
+            },
+            {
+                "awareness": 77.8,
+                "compatibility": 60.96,
+                "virality": 100.0,
+                "engagement": 56.87,
+                "image": "https://cdn.boksi.com/.../thumbnail.jpg",
+                "creator": {
+                    "name": "Rita Piironen",
+                    "username": "ritapiiii",
+                    "image": "https://cdn.boksi.com/.../thumbnail.jpg",
+                },
+            },
+            {
+                "awareness": 93.31,
+                "compatibility": 65.84,
+                "virality": 72.8,
+                "engagement": 52.19,
+                "image": "https://cdn.boksi.com/.../thumbnail.jpg",
+                "creator": {
+                    "name": "Janita Iivanainen",
+                    "username": "janitamariaw",
+                    "image": "https://cdn.boksi.com/.../thumbnail.jpg",
+                },
+            },
+        ],
+        "audience_age": {
+            "group_1": 0.084,
+            "group_2": 0.35,
+            "group_3": 0.327,
+            "group_4": 0.159,
+            "group_5": 0.061,
+            "group_6": 0.014,
+            "group_7": 0.003,
+        },
+        "countries": {"FI": 0.889, "US": 0.039},
+    }
 
     # Rendered
-    html_string = loader.render_to_string("pdfgenerator/pdf.html")
-
-    html = HTML(string=html_string)
-    # result = html.write_pdf()
-
-    html.write_pdf(
-        "./pdfs/test.pdf", stylesheets=[CSS(string="body { font-size: 16px }")]
+    html_string = loader.render_to_string(
+        # pass chart as image
+        "pdfgenerator/pdf.html",
+        {"input_data": input_data},
     )
+    html = HTML(string=html_string, base_url=request.build_absolute_uri())
 
-    # Creating http response
-    response = HttpResponse(content_type="application/pdf;")
-    response["Content-Disposition"] = "inline; filename=list_people.pdf"
-    response["Content-Transfer-Encoding"] = "binary"
+    pdf = html.write_pdf("./pdfs/test1.pdf")
 
-    # with tempfile.NamedTemporaryFile(delete=True) as output:
-    #     output.write(result)
-    #     output.flush()
-    #     output = open(output.name, "r")
-    #     response.write(output.read())
-
-    return HttpResponse(html_string)
-    # return response
+    # return HttpResponse(html_string)
+    # return HttpResponse(pdf, content_type="application/pdf")
+    return HttpResponse("pdf has been saved inside /pdf folder")
