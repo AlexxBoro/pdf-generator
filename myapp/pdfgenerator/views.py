edbf4c96df5f3c6e69a3948b424c13029edaa37c
AlexxBoro <aleks.borowska91@gmail.com>
2021-07-01 13:02:25 +0200

    better naming convention for charts, moved dictionaries into a separate file

--- myapp/pdfgenerator/views.py
+++ myapp/pdfgenerator/views.py
@@ -6,128 +6,7 @@ from django.views.decorators.csrf import csrf_exempt
 import json
 import base64
 from weasyprint import HTML
-
-input_data_1 = {
-    "name": "Wolt: Tue paikallisia ravintoloita",
-    "used_budget": 6562,
-    "budget": 3000,
-    "creators": 24,
-    "submissions": 48,
-    "applications": 242,
-    "reach": 232874,
-    "engagement": 17786,
-    "impressions": 252791,
-    "cpm": 25.96,
-    "cpe": 0.37,
-    "engagement_percentage": 10.28,
-    "followers": 271389,
-    "audience_female_share": 0.65,
-    "top_posts": [
-        {
-            "awareness": 100.0,
-            "compatibility": 100.0,
-            "virality": 51.6,
-            "engagement": 50.0,
-            "image": "pdfgenerator/images/woman_1.jpeg",
-            "creator": {
-                "name": "Jutta Häkämies",
-                "username": "juttaemmii",
-                "image": "pdfgenerator/images/avatar_1.jpeg",
-            },
-        },
-        {
-            "awareness": 77.8,
-            "compatibility": 60.96,
-            "virality": 100.0,
-            "engagement": 56.87,
-            "image": "pdfgenerator/images/woman_2.jpeg",
-            "creator": {
-                "name": "Rita Piironen",
-                "username": "ritapiiii",
-                "image": "pdfgenerator/images/avatar_2.jpeg",
-            },
-        },
-        {
-            "awareness": 93.31,
-            "compatibility": 65.84,
-            "virality": 72.8,
-            "engagement": 52.19,
-            "image": "pdfgenerator/images/woman_3.jpeg",
-            "creator": {
-                "name": "Janita Iivanainen",
-                "username": "janitamariaw",
-                "image": "pdfgenerator/images/avatar_3.jpeg",
-            },
-        },
-    ],
-    "audience_age": {
-        "group_1": 0.084,
-        "group_2": 0.35,
-        "group_3": 0.327,
-        "group_4": 0.159,
-        "group_5": 0.061,
-        "group_6": 0.014,
-        "group_7": 0.003,
-    },
-    "audience_gender": [
-        {"gender": "men", "number": 35},
-        {"gender": "women", "number": 65},
-    ],
-    "countries": {
-        "Morocco": 2,
-        "Turkey": 2,
-        "Indonesia": 2,
-        "Italy": 2,
-        "Mexico": 2,
-        "Canada": 2,
-        "Sweden": 2,
-        "Australia": 2,
-        "Russia": 2,
-        "Germany": 3,
-        "India": 3,
-        "UK": 3,
-        "Brazil": 4,
-        "US": 5,
-        "Finland": 90,
-    },
-}
-
-input_data_2 = {
-    "items": [
-        {
-            "name": "item A",
-            "description": "item description",
-            "quantity": 2,
-            "unit_price": "$100",
-            "total_price": "$200",
-        },
-        {
-            "name": "item B",
-            "description": "item description item description longer description",
-            "quantity": 1,
-            "unit_price": "$100",
-            "total_price": "$100",
-        },
-        {
-            "name": "item C",
-            "description": "item description decs",
-            "quantity": 1,
-            "unit_price": "$100",
-            "total_price": "$100",
-        },
-        {
-            "name": "item D",
-            "description": "item description item description longer",
-            "quantity": 1,
-            "unit_price": "$100",
-            "total_price": "$100",
-        },
-    ],
-    "subtotal": "$500",
-    "tax_rate": "20%",
-    "total_tax": "$100",
-    "total": "$600",
-}
+from .dictionaries import input_data_1, input_data_1
 
 
 def get_data(request):
@@ -145,13 +24,12 @@ def generate_pdf(request):
     """Generate PDF 1."""
 
     # parsing 'request.body' from POST into JSON
-    body_unicode = request.body.decode("utf-8")
-    body = json.loads(body_unicode)
+    body = json.loads(request.body.decode("utf-8"))
     content = body
 
-    chart_1 = content["chart_1"]
-    chart_2 = content["chart_2"]
-    chart_3 = content["chart_3"]
+    bar_chart = content["bar_chart"]
+    horizontal_bar_chart = content["horizontal_bar_chart"]
+    pie_chart = content["pie_chart"]
 
     html_string = loader.render_to_string(
         "pdfgenerator/template_1.1.html",
@@ -163,11 +41,15 @@ def generate_pdf(request):
     )
     html_string3 = loader.render_to_string(
         "pdfgenerator/template_1.3.html",
-        {"input_data": input_data_1, "chart_1": chart_1},
+        {"input_data": input_data_1, "bar_chart": bar_chart},
     )
     html_string4 = loader.render_to_string(
         "pdfgenerator/template_1.4.html",
-        {"input_data": input_data_1, "chart_2": chart_2, "chart_3": chart_3},
+        {
+            "input_data": input_data_1,
+            "horizontal_bar_chart": horizontal_bar_chart,
+            "pie_chart": pie_chart,
+        },
     )
 
     html = HTML(string=html_string, base_url=request.build_absolute_uri())
